dcd4d7c3ce27365ffc2ebb71633a313213e0f0ba
Dimafg5 <dmitriu4477@gmail.com>
2025-07-22 17:32:32 +0000

    modified

--- tests/filtr.spec.ts
+++ tests/filtr.spec.ts
@@ -3,7 +3,11 @@ import { faker } from "@faker-js/faker";
 
 test("Проверка работи фиььтра", async ({ page }) => {
   await page.goto("https://automationexercise.com/");
-  await page.getByRole("button", { name: "Consent" }).click();
+  const consentButton = page.getByRole("button", { name: "Consent" });
+if (await consentButton.isVisible({ timeout: 5000 }).catch(() => false)) {
+  await consentButton.click();
+}
+  //await page.getByRole("button", { name: "Consent" }).click();
   await page.getByRole("link", { name: "Products" }).click();
   await page.getByRole("link", { name: " Men" }).click();
   await page.getByRole("link", { name: "TSHIRTS" }).click();
